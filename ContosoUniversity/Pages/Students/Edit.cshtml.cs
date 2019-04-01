39d55ad2295ee276e2d78e736266dbe81e733fd0
samanthahancock <han13018@byui.edu>
2019-03-31 21:31:38 -0600

    Step 2 completed

--- ContosoUniversity/Pages/Students/Edit.cshtml.cs
+++ ContosoUniversity/Pages/Students/Edit.cshtml.cs
@@ -29,7 +29,7 @@ namespace ContosoUniversity.Pages.Students
                 return NotFound();
             }
 
-            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+            Student = await _context.Student.FindAsync(id);
 
             if (Student == null)
             {
@@ -38,33 +38,47 @@ namespace ContosoUniversity.Pages.Students
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            _context.Attach(Student).State = EntityState.Modified;
+            var studentToUpdate = await _context.Student.FindAsync(id);
 
-            try
+            if (await TryUpdateModelAsync<Student>(
+            studentToUpdate,
+            "student",
+            s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
             {
                 await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(Student.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return RedirectToPage("./Index");
             }
 
-            return RedirectToPage("./Index");
+            return Page();
         }
+
+        /*   try
+           {
+               await _context.SaveChangesAsync();
+           }
+           catch (DbUpdateConcurrencyException)
+           {
+               if (!StudentExists(Student.ID))
+               {
+                   return NotFound();
+               }
+               else
+               {
+                   throw;
+               }
+           }
+
+           return RedirectToPage("./Index");
+       }
+
+   */
 
         private bool StudentExists(int id)
         {
